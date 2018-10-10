0e408b3584d84f55d69bc38e4fcb868e693a92f9
Mikhail <mikhail@smarttouchinteractive.com>
2018-10-10 15:48:48 +0300

    comment 1 by new_br_2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //    comment 1 by new_br_2
         }
     }
 }
