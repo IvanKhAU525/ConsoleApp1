b0b56d7081359099b83f2c6f12583b8544f80e41
Mikhail <mikhail@smarttouchinteractive.com>
2018-10-10 15:57:53 +0300

    clear master

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,8 +7,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //    comment 1 by master
-            //    comment 2 by master
         }
     }
 }
